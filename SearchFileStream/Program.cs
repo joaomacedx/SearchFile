b2016e29c94a2ea43b1129c586f75c2cfe44710a
João Macêdo <99701121+joaomacedx@users.noreply.github.com>
2022-06-26 20:57:39 -0300

    Adicionar arquivos de projeto.

---
+++ SearchFileStream/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SearchFileStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string disco = GetDiscoFileStream();
+            Console.WriteLine(disco);
+        }
+        public static string GetDiscoFileStream()
+        {
+            string caminho = "";
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            List<string> listaDisco = new List<string>();
+            foreach (DriveInfo d in allDrives)
+            {
+                //Console.WriteLine("Drive {0}", d.Name);
+                listaDisco.Add(d.Name);
+            }
+
+            foreach (string disco in listaDisco)
+            {
+                foreach (string diretorio in Directory.GetDirectories(disco))
+                {
+                    string dirBusca = diretorio.Remove(0, disco.Length);
+                    if (dirBusca == "Drives compartilhados")
+                    {
+                        caminho = disco + dirBusca;
+                        //Console.WriteLine("O file stream foi encontrado no disco: " + disco);
+                        //Console.WriteLine(caminho);
+                        return disco;
+
+                    }
+                }
+            }
+            return "Disco não encontrado!";
+        }
+    }
+}
